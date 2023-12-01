7101efafbce67dd3d93e0a3b1965b57c81b66d13
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 22:32:26 +0700

    bo sung man hinh 2

---
+++ Screen02.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import config from './config';
+import Product from './src/components/product/Product';
+
+//export
+export default function Screen02() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+
+        async function getData() {
+            const rest = await fetch(`${config.ENDPOINT}/shops`);
+            const data = await rest.json();
+
+            setProducts(data);
+        }
+
+        getData();
+    }, []);
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={styles.contentList}>
+                {products.map((item) => (
+                    <Product item={item} key={item.id} />
+                ))}
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 14,
+        lineHeight: 22,
+    },
+    contentList: {
+        gap: 20,
+        paddingHorizontal: 20,
+    },
+});
